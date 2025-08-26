6490febb25f8b9c00cfa1db9a452d01e39efc976
Spencer Stephen <spncrstphn14@gmail.com>
2025-08-25 18:19:47 -0700

    fix paths to window and start on cop interaction

--- states/cops_state.gd
+++ states/cops_state.gd
@@ -1,6 +1,8 @@
 class_name CopsState
 extends BaseState
 
+var cop_scene := load("res://actors/cop.tscn")
+var cop = cop_scene.instantiate()
 
 func enter() -> void:
 	# Change the screen text -- make into global variable?--
@@ -8,12 +10,15 @@ func enter() -> void:
 	get_tree().current_scene.get_node("ScreenText").visible = true
 	await get_tree().create_timer(2.0).timeout # Waits for 2 seconds
 	get_tree().current_scene.get_node("ScreenText").visible = false
-	
-	
-	var cop_scene := load("res://actors/cop.tscn")
-	var cop = cop_scene.instantiate()
-	
+
 	get_tree().current_scene.add_child(cop)
+	cop.get_node("Path3D/PathFollow3D").path_completed.connect(_on_path_completed)
+
+func update(delta: float) -> void:
+	pass
 	
+func _on_path_completed() -> void:
+	print("test")
+
 func exit() -> void:
 	get_tree().current_scene.get_node("ScreenText").visible = false
